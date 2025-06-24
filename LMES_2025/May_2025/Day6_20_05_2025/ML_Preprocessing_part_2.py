2ca75adbdfc6795d6ab8457525c3f7ce9f706e79
SysSyncer <harikrishnan4665@gmail.com>
2025-06-24 08:05:52 +0530

    .

---
+++ LMES_2025/May_2025/Day6_20_05_2025/ML_Preprocessing_part_2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("diamonds.csv")
+print(df.head())
+
+finding_null_count = df.isna().sum()
+print(finding_null_count)
+
+df.drop("Unnamed: 0", axis=1, inplace=True)
+print(df.head())
+
+df.drop(["carat", "cut"], axis=1, inplace=True)
+print(df.head())
+
+"""
+axis=1 - This is basically to remove on column wise.
+inplace=True - This is basically like save rather than creating and returning a new updated copy.
+(Ensuring removal happened properly)
+"""
